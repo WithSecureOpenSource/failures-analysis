b5bc601043d55155986d60366c9ec50d7af60ae0
Tatu Aalto <tatu.aalto@f-secure.com>
2022-05-24 18:23:21 +0300

    More linting tooling

--- tasks.py
+++ tasks.py
@@ -3,6 +3,8 @@ from invoke import task
 
 @task
 def lint(ctx):
-    ctx.run("mypy --show-error-codes utest failure_analysis")
     ctx.run("isort failure_analysis")
     ctx.run("black --config pyproject.toml utest failure_analysis")
+    ctx.run("flake8 utest failure_analysis")
+    ctx.run("mypy --show-error-codes utest failure_analysis")
+
